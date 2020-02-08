3529985f73eec67fcdd07b61353253bc2699ac57
chrisxcoto <53847773+chrisxcoto@users.noreply.github.com>
2020-02-08 10:29:53 -0500

    chaged route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes")(app);
+require("./routing/apiroutes")(app);
 require("./routing/htmlRoutes")(app);
 
 
